d52e4d12cba2f5fd3dd189ba0ed766b2ecd5632a
Adnan Tariq <adnan6official@gmail.com>
2024-03-27 21:49:04 +0500

    created backend of user registration

--- backend/controller/auth-controller.js
+++ backend/controller/auth-controller.js
@@ -1,3 +1,4 @@
+const User = require("../models/user-model");
 /*
 What is controller?
 In express.js controller are the part of our code that is responsible for handling application logic. Controllers are typically used to process incomming request, interact with  models and sent responses back to the client. They help to organize our application by separating concerns and following the mvc (Model-View_controller) design system
@@ -5,21 +6,27 @@ In express.js controller are the part of our code that is responsible for handli
 
 const home = async (req, res) => {
   try {
-    res.status(200).json({message : "Home page"});
+    res.status(200).json({ message: "Home page" });
   } catch (error) {
-    res.status(400).json("Page not found")
+    res.status(400).json("Page not found");
   }
 };
 
 const register = async (req, res) => {
   try {
+    const { username, email, phone, password } = req.body;
+    const userExist = await User.findOne({ email });
+
+    if (userExist) {
+      return res.status(400).json({ message: "User already exist" });
+    }
+    const userCreated = await User.create({ username, email, phone, password });
+
     console.log(req.body);
-    res.status(200).json({message : req.body});
+    res.status(200).json({ message: userCreated });
   } catch (error) {
-    res.status(400).json("Page not found")
+    res.status(400).json("Page not found");
   }
 };
 
-
-
-module.exports = {home , register}
+module.exports = { home, register };
--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,15 +1,14 @@
 const mongoose = require("mongoose");
+const url = process.env.MONGODB_URI;
 // const url = "mongodb://127.0.0.1:27017/thapa_project";
 
-const url = process.env.MONGODB_URI;
-
-const dbConnect = async () => {
+const dbConnection = async () => {
   try {
     await mongoose.connect(url);
-    console.log("Db connected");
+    console.log("Connected to database");
   } catch (error) {
-    console.log("Db not connected");
+    console.log("Db not connected:", error);
   }
 };
 
-module.exports = dbConnect;
+module.exports = dbConnection;
